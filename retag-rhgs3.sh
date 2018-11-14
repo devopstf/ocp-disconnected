5b73147c92c0399c4a6ae6f7189171b45aab6632
root <root@localhost.localdomain>
2018-11-14 08:39:09 +0000

    Testing vars population from registered task output

--- retag-rhgs3.sh
+++ retag-rhgs3.sh
@@ -14,7 +14,7 @@ done
 
 for line in $filelines ; do
     version=$(docker inspect $registry/$line | grep -m1 version | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//') && \
-    sh -c "docker tag $registry/$line:$tag $local/$line:$version" && \
-    sh -c "docker push $local/$line:$version" && \
-    sh -c "docker rmi $registry/$line:$tag"
+    docker tag $registry/$line:$tag $local/$line:$version && \
+    docker push $local/$line:$version && \
+    docker rmi $registry/$line:$tag
 done
